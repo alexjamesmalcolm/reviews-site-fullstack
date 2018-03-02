312df36b8a06a6c18f711ab3110c1c05825bb451
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-02 10:40:12 -0500

    Created test shouldHaveNameBad

--- src/test/java/org/wecancodeit/reviewssitefullstack/TagTest.java
+++ src/test/java/org/wecancodeit/reviewssitefullstack/TagTest.java
@@ -16,4 +16,12 @@ public class TagTest {
 		String actual = underTest.getName();
 		assertThat(actual, is(name));
 	}
+	
+	@Test
+	public void shouldHaveNameBad() {
+		String name = "Bad";
+		Tag underTest = new Tag(name);
+		String actual = underTest.getName();
+		assertThat(actual, is(name));
+	}
 }
