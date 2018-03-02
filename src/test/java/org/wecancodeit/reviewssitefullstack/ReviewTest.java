2dbe090015ac1f320aaacca907b02d4e5dedafb4
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-02 10:42:40 -0500

    Created test shouldHaveTitleStarWars

---
+++ src/test/java/org/wecancodeit/reviewssitefullstack/ReviewTest.java
@@ -0,0 +1,19 @@
+package org.wecancodeit.reviewssitefullstack;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReviewTest {
+	
+	@Test
+	public void shouldHaveTitleStarWars() {
+		String title = "Star Wars";
+		Review underTest = new Review(title);
+		String actual = underTest.getTitle();
+		assertThat(actual, is(title));
+	}
+}
