572cfb386e4cd563134263fc8c99e46a098c2cca
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-04 18:13:17 -0500

    shouldRemoveDoubleQuotes

---
+++ src/main/java/org/wecancodeit/reviewssitefullstack/CSVReader.java
@@ -0,0 +1,50 @@
+package org.wecancodeit.reviewssitefullstack;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader {
+
+	public static void main(String[] args) {
+		// String file = "7817_1.csv";
+		// try {
+		// BufferedReader br = new BufferedReader(new FileReader(file));
+		// } catch (FileNotFoundException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		//
+		String csvFile = "consumer-reviews-of-amazon-products-QueryResult.csv";
+		String line = "";
+		String cvsSplitBy = ",";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+			while ((line = br.readLine()) != null) {
+
+				// use comma as separator
+				String[] country = line.split(cvsSplitBy);
+
+				for (int i = 0; i < country.length; i++) {
+					if (i == 0) {
+						String item = country[i];
+						System.out.print(item);
+						System.out.print(" ");
+					}
+				}
+				System.out.println();
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static String removeDoubleQuotes(String input) {
+		return "test";
+	}
+
+}
---
+++ src/test/java/org/wecancodeit/reviewssitefullstack/CSVReaderTest.java
@@ -0,0 +1,17 @@
+package org.wecancodeit.reviewssitefullstack;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.wecancodeit.reviewssitefullstack.CSVReader.removeDoubleQuotes;
+
+import org.junit.Test;
+
+public class CSVReaderTest {
+	
+	@Test
+	public void shouldRemoveDoubleQuotes() {
+		String input = "te\"st";
+		String output = removeDoubleQuotes(input);
+		assertThat(output, is("test"));
+	}
+}
