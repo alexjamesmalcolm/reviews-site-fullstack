7f98a77c259fbb8b5a204793259a4d33f607ebfb
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-17 17:41:13 -0400

    Did I make three tests pass without changing production code? Yeah, what are you going to do about it More?

--- src/test/java/org/wecancodeit/reviewssitefullstack/ReviewSiteControllerMockMvcTest.java
+++ src/test/java/org/wecancodeit/reviewssitefullstack/ReviewSiteControllerMockMvcTest.java
@@ -1,14 +1,17 @@
 package org.wecancodeit.reviewssitefullstack;
 
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import javax.annotation.Resource;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -29,6 +32,27 @@ public class ReviewSiteControllerMockMvcTest {
 	
 	@MockBean
 	private TagRepository tagRepo;
+	
+	@Mock
+	Tag tag;
+	
+	@Mock
+	Review review;
+	
+	@Mock
+	Category category;
+	
+	long tagId = 1L;
+	long reviewId = 1L;
+	long categoryId = 1L;
+	
+	@Before
+	public void setup() {
+		when(tagRepo.findOne(tagId)).thenReturn(tag);
+		when(categoryRepo.findOne(categoryId)).thenReturn(category);
+		when(reviewRepo.findOne(reviewId)).thenReturn(review);
+		when(review.getCategory()).thenReturn(category);
+	}
 	
 	@Test
 	public void shouldBeOk() throws Exception {
