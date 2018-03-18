34707c7f24d3e722f2656edbe3ddc137239628bd
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-18 15:54:52 -0400

    shouldHaveDateTime

--- src/main/java/org/wecancodeit/reviewssitefullstack/Comment.java
+++ src/main/java/org/wecancodeit/reviewssitefullstack/Comment.java
@@ -1,15 +1,23 @@
 package org.wecancodeit.reviewssitefullstack;
 
+import java.util.Date;
+
 public class Comment {
 
 	private String content;
+	private Date date;
 
-	public Comment(String content) {
+	public Comment(String content, Date date) {
 		this.content = content;
+		this.date = date;
 	}
 
 	public String getContent() {
 		return content;
+	}
+
+	public Date getDate() {
+		return date;
 	}
 
 }
--- src/test/java/org/wecancodeit/reviewssitefullstack/CommentTest.java
+++ src/test/java/org/wecancodeit/reviewssitefullstack/CommentTest.java
@@ -3,6 +3,8 @@ package org.wecancodeit.reviewssitefullstack;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.Date;
+
 import org.junit.Test;
 
 public class CommentTest {
@@ -10,15 +12,23 @@ public class CommentTest {
 	@Test
 	public void shouldHaveContentFoo() {
 		String content = "Foo";
-		Comment underTest = new Comment(content);
+		Comment underTest = new Comment(content, new Date());
 		assertThat(underTest.getContent(), is(content));
 	}
 
 	@Test
 	public void shouldHaveContentBar() {
 		String content = "Bar";
-		Comment underTest = new Comment(content);
+		Comment underTest = new Comment(content, new Date());
 		String actual = underTest.getContent();
 		assertThat(actual, is(content));
+	}
+	
+	@Test
+	public void shouldHaveDateTime() {
+		Date date = new Date();
+		Comment underTest = new Comment("", date);
+		Date actual = underTest.getDate();
+		assertThat(actual, is(date));
 	}
 }
